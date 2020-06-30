9212af2d5fe34434d3d134c98f2c32c4c93038eb
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2020-06-30 17:03:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
+
 // Create Resource Group
 resource "azurerm_resource_group" "rg_network" {
   name                = "${var.rg_network}"
