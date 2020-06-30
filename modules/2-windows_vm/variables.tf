3854498b706abaa3acc2652ad59967c0bb669b63
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2020-06-30 17:08:27 +0530

    Update variables.tf

--- modules/2-windows_vm/variables.tf
+++ modules/2-windows_vm/variables.tf
@@ -21,24 +21,19 @@ variable "admin_password" {
 
 variable "vmsize" {
   description = "VM Size for the Production Environment"
-  type        = "map"
+  type        = "string"
+  default = "Standard_D2s_v3"
 
-    default = {
-      small         =   "Standard_DS1_v2"
-      medium        =   "Standard_D2s_v3"
-      large         =   "Standard_D4s_v3"
-      extralarge    =   "Standard_D8s_v3"
-    }
 }
 
 variable "os_ms" {
-  description = "Operating System for Database (MSSQL) on the Production Environment"
+  description = "Operating System for dev Environment"
   type        = "map"
 
-    default = {
-      publisher   =   "MicrosoftWindowsServer"
-      offer       =   "WindowsServer"
-      sku         =   "2019-Datacenter"
-      version     =   "latest"
-    }
+      default = {
+        publisher   =   "MicrosoftWindowsServer"
+        offer       =   "Windows-10"
+        sku         =   "19h1-evd"
+        version     =   "latest"
+      }
 }
